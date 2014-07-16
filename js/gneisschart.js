26f637e949d8a00b5ef98c4c877b0bafb418011b
Chen Wu <wuchencarol@gmail.com>
2014-07-16 16:02:58 -0400

    Update gneisschart.js

--- js/gneisschart.js
+++ js/gneisschart.js
@@ -256,7 +256,33 @@ Gneiss.helper = {
     var separator = elem.attr("transform").indexOf(",") > -1 ? "," : " ";
     var trans = elem.attr("transform").split(separator);
     return { x: (trans[0] ? parseFloat(trans[0].split("(")[1]) : 0), y: (trans[1] ? parseFloat(trans[1].split(")")[0] ): 0) };
-  }
+  },
+  wrap: function(text, size) {
+		//from http://bl.ocks.org/mbostock/7555321
+		text.each(function() {
+			var text = d3.select(this),
+				words = text.text().split(/[\s]+/).reverse(),
+				word,
+				line = [],
+				lineNumber = 0,
+				lineHeight = 1.2, // ems
+				y = text.attr("y"),
+				x = text.attr("x"),
+				dy = parseFloat(text.attr("dy")) || 0,
+				tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+				text.attr("dy",null)
+			while (word = words.pop()) {
+				line.push(word);
+				tspan.text(line.join(" "));
+				if (tspan.node().getComputedTextLength() > size) {
+					line.pop();
+					tspan.text(line.join(" "));
+					line = [word];
+					tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", lineHeight + "em").text(word);
+				}
+			}
+		});
+	}
 };
 
 function Gneiss(config)
